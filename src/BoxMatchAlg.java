e340b5e28c7d2fd39eddabdcbcf3d4cd32f4ff9c
ahmettavus <ahmettavus40@gmail.com>
2022-09-29 22:58:12 +0200

    Box Match

--- src/BoxMatchAlg.java
+++ src/BoxMatchAlg.java
@@ -2,7 +2,7 @@ public class BoxMatchAlg {
     public static void main(String[] args) {
 
         Fighter f1 = new Fighter("Ahmet" , 10 , 120 , 90, 10);
-        Fighter f2 = new Fighter("Sakine" , 20 , 90 , 85,20);
+        Fighter f2 = new Fighter("Mehmet" , 20 , 90 , 85,20);
 
         Match match = new Match(f1,f2,80,100);
         match.run();
